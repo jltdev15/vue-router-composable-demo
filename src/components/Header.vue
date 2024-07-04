a650326157272b9e95a94f5e7d627f55e532e6e9
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2024-07-04 22:26:28 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <div>
+                    <h1>Demo Vue router</h1>
+                </div>
+                <ul>
+                    <router-link to='/'>Home</router-link>
+                    <router-link to='/about'>About</router-link>
+                    <router-link to='/login'>Login</router-link>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+header {
+    background-color: brown;
+    color: #fff;
+}
+nav {
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
---
+++ src/views/AboutPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>This is About Page</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
